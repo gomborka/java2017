b4ad0de539b9dcc2148bcc19b1d7a56f8fff6ab5
borka-PC\borka <boris.goman@gmail.com>
2017-09-03 22:47:04 +0300

    projZoo

---
+++ ZooProject_Boris/src/com/company/zooManage/Cage.java
@@ -0,0 +1,16 @@
+package com.company.zooManage;
+
+import com.company.animal.Animal;
+
+import java.util.Arrays;
+
+/**
+ * Created by borka on 9/2/2017.
+ */
+public class Cage {
+
+    public static Animal [] airCage = new Animal[10];
+    public static Animal [] groundCage = new Animal[10];
+    public static Animal [] waterCage = new Animal[10];
+
+}
