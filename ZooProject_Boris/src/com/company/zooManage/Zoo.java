ca09482e013c9f75b41af068bd08fc80d219e8ef
borka-PC\borka <boris.goman@gmail.com>
2017-09-04 20:45:05 +0300

    little update for zoo

--- ZooProject_Boris/src/com/company/zooManage/Zoo.java
+++ ZooProject_Boris/src/com/company/zooManage/Zoo.java
@@ -7,59 +7,62 @@ import com.company.animal.Animal;
  */
 public class Zoo {
 
-
+    Cage[] cage = new Cage[3];
 
     public Zoo() {
-         Animal [] airCage = new Animal[10];
-       Animal [] groundCage = new Animal[10];
-         Animal [] waterCage = new Animal[10];
+        Animal[] airCage;
+        Animal[] groundCage;
+        Animal[] waterCage;
 
     }
 
 
-
-
-    public void addAnimalToZoo(Animal animal)
-    {
-        switch (animal.getPlace())
-        {
-            case "ground" :
+    public void addAnimalToZoo(Animal animal) {
+        switch (animal.getPlace()) {
+            case "ground":
                 addAnimalToCage(animal, Cage.groundCage);
-            break;
-            case "water" :
-               addAnimalToCage(animal,Cage.waterCage);
                 break;
-            case "air" :
-                addAnimalToCage(animal,Cage.airCage);
+            case "water":
+                addAnimalToCage(animal, Cage.waterCage);
+                break;
+            case "air":
+                addAnimalToCage(animal, Cage.airCage);
                 break;
         }
     }
 
-    public void addAnimalToCage (Animal animal, Animal [] cage)
-    {
-       for (int i=0;i< cage.length; i++)
-      {
-          if (cage [i] == null )
-           {
-               cage [i] = animal;
-               return;
-           }
-             }
+    public void addAnimalToCage(Animal animal, Animal[] cage) {
+        for (int i = 0; i < cage.length; i++) {
+            if (cage[i] == null) {
+                cage[i] = animal;
+                return;
+            }
+        }
     }
 
-   public static void printCage(Animal [] cage)
-   {
+    public static void printCage(Animal[] cage) {
 
 
-       System.out.println(cage[0].getPlace().toString() +" animal:");
+ //       int busyCages = 0;
+//       for (Animal curr: cage
+//            ) {
+//           if (curr != null) {
+//               System.out.println(curr);
+//               busyCages++;
+//           }
+//       }
+        System.out.println(cage[0].getPlace().toString() + " animal:");
+        for (int i = 0; i < cage.length; i++) {
 
-       for (Animal curr: cage
-            ) {
-           if (curr != null) {
-               System.out.println(curr);
-           }
-       }
-       }
+            if (cage[i] != null) {
+                System.out.println(cage[i].getName() + " - lives in #" + (i + 1) + " "+ (cage[i].getPlace().toString() + " cage"));
+              //  busyCages++;
+            }
+
+            //   System.out.println(cage[0].getPlace().toString() +" cage live "+ busyCages + " animal" );
+
+        }
+        System.out.println("-----------------");
+    }
 
 }
-
