532eca151a477121f834dc6ccfc86558d77b4f56
DESKTOP-BO5RVI6\Comp14 <boris.goman@gmail.com>
2017-09-19 12:54:03 +0300

    sevrver-client

--- SocketServer/src/com/borka/Main.java
+++ SocketServer/src/com/borka/Main.java
@@ -1,8 +1,28 @@
 package com.borka;
 
+import com.sun.security.ntlm.Client;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        try {
+            ServerSocket serverSocket = new ServerSocket(5000);
+            Socket client=null;
+            while(true) {
+
+                System.out.println("Waiting for connection...");
+               client= serverSocket.accept();
+                System.out.println("connected " + client.getInetAddress().getHostName() +  " " + client.isConnected());
+              // new ClientThread(newClient).start();
+               ClientThread newClient =new ClientThread(client);
+                newClient.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ clientServer/src/com/borka/Main.java
@@ -0,0 +1,33 @@
+package com.borka;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Main {
+    private static String message;
+
+    public static void main(String[] args) {
+	String ip = "10.0.0.5";
+	int port =5000;
+        OutputStream out=null ;
+
+        try {
+            while(true) {
+                Socket client = new Socket(ip, port);
+               Scanner scanner = new Scanner(System.in);
+                message = scanner.nextLine();
+          //      message="Welocme ";
+                out = client.getOutputStream();
+                while (!message.equals("end")) {
+                    out.write(message.getBytes());
+                }
+                 out.close();
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
